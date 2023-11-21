b17d6fafe74f48c69d93d6cd469c33a589d9c3ea
Kiyotaka Abe <kiyo7447@gmaill.com>
2023-11-21 22:25:13 +0900

    update

---
+++ TheHousekeeperAndTheProfessor/kaprekar2.py
@@ -0,0 +1,17 @@
+﻿def is_kaprekar_number(n):
+    if n == 1:
+        return True
+    else:
+        square = str(n**2)
+        for i in range(1, len(square)):
+            left = int(square[:i])
+            right = int(square[i:])
+            if left != 0 and right != 0 and left + right == n:
+                return True
+    return False
+
+
+i = 6174
+print(f'Is {i} a Kaprekar number? {is_kaprekar_number(i)}')
+
+
