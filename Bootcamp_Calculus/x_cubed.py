923125a8b40e9cd4c14f1a975a83506452574f40
Kiyotaka Abe <kiyo7447@gmaill.com>
2023-11-24 18:10:27 +0900

    add calculus

---
+++ Bootcamp_Calculus/x_cubed.py
@@ -0,0 +1,15 @@
+﻿#
+import matplotlib.pyplot as plt
+import numpy as np
+
+def x3(x):
+    return np.power(x, 3)
+
+# -5から5まで100個のデータを生成する
+x = np.linspace(-10, 10,1000)
+y = x3(x)
+
+# グラフを描画する
+plt.plot(x, y, label="$x^3$")
+plt.legend() # 凡例を表示する
+plt.show()
