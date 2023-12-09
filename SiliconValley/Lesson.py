a9ae990b25070245286ef13dbb976caf35141226
Kiyotaka Abe <kiyo7447@gmaill.com>
2023-12-09 22:25:14 +0900

    update

---
+++ SiliconValley/Lesson.py
@@ -0,0 +1,24 @@
+﻿#lesson 1
+print("Hello, World!")
+
+print('Hi', 'Mike', sep=',', end='.\n')
+
+import math
+print(help(math))
+
+print(r'c:\name\name')
+
+print("""\
+line1
+line2
+line3\
+""")
+
+#lesson 2
+print('Hi.' * 3 + 'Mike.')
+
+## 逆順に表示する
+l = [1, 20, 4, 50, 2, 1, 2]
+print(l[::-1])
+
+#
---
+++ SiliconValley/README.md
@@ -0,0 +1,5 @@
+﻿# シリコンバレー一流プログラマーが教える Pythonプロフェッショナル大全
+## PyCharmのインストール
+### PyCharmのダウンロード
+[PyCharmのダウンロード](https://www.jetbrains.com/pycharm/download/#section=windows)
+
